99f4d5692cf58d1ff910207eaf1200f7a5a203a6
Marco 9726 <marcosarra9@gmail.com>
2023-05-12 11:37:55 +0200

    init commit

---
+++ src/org/lessons/java/christmas/Main.java
@@ -0,0 +1,12 @@
+package org.lessons.java.christmas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		List <String> wishList = new ArrayList<>();
+	}
+}
