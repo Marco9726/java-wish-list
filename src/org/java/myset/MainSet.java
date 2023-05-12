5e5ffa7b726787d7d10bf114b2bdb9e365c4f5a7
Marco 9726 <marcosarra9@gmail.com>
2023-05-12 12:48:22 +0200

    Snack 2 svolto

--- src/org/java/myset/MainSet.java
+++ src/org/java/myset/MainSet.java
@@ -1,9 +1,27 @@
 package org.java.myset;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class MainSet {
 	
 	public static void main(String[] args) {
 		
+		Set<Integer> mySet = new HashSet<>();
 		
+		Random r = new Random();
+		
+		
+		
+		while ( mySet.size() < 5) {
+			
+			int randomNumber = r.nextInt(10 + 1) + 2;
+			
+			mySet.add(randomNumber);
+			
+		}
+		
+		System.out.println(mySet);
 	}
 }
